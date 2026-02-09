90ba4f3a8b9f24e571bb96d62e81f46960d59982
Ranjitha S T <ranjithast009@gmail.com>
2026-02-09 17:47:29 +0530

    Employee class added

---
+++ Person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self):
+        self.personID=None
+        self.personName=None
+p=Person()
+p.personID=100
+p.personName="Jay"
+print(p)
+print(p.personID)
+print(p.personName)
+p1=Person()
+p1.personID=101
+p1.personName="Harsh"
+print(p1)
+print(p1.personID)
+print(p1.personName)

4ab9ab27f98d58f56ba3d41a26258523b98184c8
Ranjitha S T <ranjithast009@gmail.com>
2026-02-11 11:09:18 +0530

    Patterns

--- Problems and DSA/pattern.py
+++ Problems and DSA/pattern.py
@@ -4,6 +4,7 @@
 #print pattern inside the inner loop
 #observe symmetry(optional)
 
+
 '''
 Docstring for Problems and DSA.pattern
 **** 0->4 stars
@@ -12,8 +13,8 @@ Docstring for Problems and DSA.pattern
 **** 3->4
 '''
 def printPatter1(N):
-    for i in range(0,N):
-        for j in range(0,N):
+    for i in range(N):
+        for j in range(N):
             print("*",end=" ")
         print(" ")
 printPatter1(4)
@@ -27,25 +28,11 @@ Docstring for Problems and DSA.pattern
 ****    3->4
 '''
 def printPatter2(N):
-    for i in range(0,N):
-        for j in range(0,i):
+    for i in range(N+1):
+        for j in range(i):
             print("*",end=" ")
         print(" ")
 printPatter2(4)
-
-'''
-Docstring for Problems and DSA.pattern
-1       0->1 stars
-12      1->2
-123     2->3
-1234    3->4
-'''
-def printPatter3(N):
-    for i in range(1,N+1):
-        for j in range(1,i+1):
-            print(j,end=" ")
-        print(" ")
-printPatter3(4)
 
 '''
 Docstring for Problems and DSA.pattern
@@ -74,9 +61,71 @@ def printPatter4(N):
             print(i,end=" ")
         print(" ")
 printPatter4(4)
+print(" ")
+
+'''
+Docstring for Problems and DSA.pattern
+**** 1-4 (4-i=1)+1=4 stars in row
+***  2-3
+**   3-2
+*    4-1
+'''
+def printPattern5(N):
+    for i in range(N+1):
+        for j in range(1,N-i+1):
+            print("*",end=" ")
+        print(" ")
+printPattern5(4)
+
+def printPattern6(N):
+    for i in range(N,0,-1):
+        for j in range(i):
+            print("*",end=" ")
+        print(" ")
+printPattern6(4)
 
 
+'''
+Docstring for Problems and DSA.pattern
+12345 1-5 (4-i=1)+1=4 stars in row
+1234  2-4
+123   3-3
+12    4-2
+1     5-1
+'''
 
+def printPattern7(N):
+    for i in range(N,0,-1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print(" ")
+printPattern7(5)
+
+'''
+Docstring for Problems and DSA.pattern
+SPACE-STARS-SPACE
+  *     0-2,1,2
+ ***    1-1,3,1
+*****   2-0,5,0
+Type	    Spaces      Stars
+0-index	    N-i-1	    2*i+1
+1-index	    N-i	        2*i-1
+'''
+
+def printPattern8(N):
+    for i in range(N):
+        for j in range(N-i-1):
+            print(" ",end=" ")
+        for j in range(2*i+1):
+             print("*",end=" ")
+        #for j in range(N-i+1):
+         #   print(" ",end=" ")
+        print(" ")
+printPattern8(5)
+
+'''
+'''
+def printPattern9()
 
 
 
